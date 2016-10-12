e8eae75a5d63449ad2833013ae2a8595adb70811
agus <licleu@gmail.com>
2016-10-12 11:39:11 -0300

    Vista de cervezas

--- resources/views/beers/listingview.blade.php
+++ resources/views/beers/listingview.blade.php
@@ -1,12 +1,18 @@
-<div class="card hoverable small">
-  <div class="card-image">
-    <img src="{{ $beer->photo_url }}">
-    <span class="card-title">{{ $beer->name }}</span>
-  </div>
-  <div class="card-content">
-    <p>{{ $beer->description }}</p>
-  </div>
-  <div class="card-action">
-    <a href="/beer/{{ $beer->slug }}">{{ $beer->name }}</a>
-  </div>
-</div>
+<div class="row">
+        <div class="col s6">
+          <div class="card small">
+            <div class="card-image">
+              <img src="{{ $beer->photo_url }}">
+              <span class="card-title">{{ $beer->name }}</span>
+            </div>
+            <div class="card-content">
+             <p>{{ $beer->description }}</p>
+            </div>
+            <div class="card-action">
+              <a href="/cerveza/{{ $beer->slug }}">{{ $beer->name }}</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
