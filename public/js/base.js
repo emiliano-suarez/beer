6090c129f645e75f30dfda47d4f3807542d4bc33
Emiliano Suárez <emilianosuarez@gmail.com>
2016-10-12 11:07:08 -0300

    change side nav with

--- public/js/base.js
+++ public/js/base.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.button-collapse').sideNav({
-    menuWidth: 400, // Default is 240
+    menuWidth: 280, // Default is 240
     edge: 'left', // Choose the horizontal origin
     closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     }
