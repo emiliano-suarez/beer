08de4bf966d7c00f14229b4557181cfc0f4ea9cd
Emiliano Suárez <emilianosuarez@gmail.com>
2016-10-18 15:29:20 -0300

    testing map view

--- resources/views/bars/detail.blade.php
+++ resources/views/bars/detail.blade.php
@@ -32,6 +32,11 @@
             <div class="card">
                 <div class="card-image">
                     <img class="activator" src="https://maps.googleapis.com/maps/api/staticmap?zoom=17&size=400x250&maptype=roadmap&language=es&markers=color:0x1B5E20|-34.5717947,-58.4313896&key=AIzaSyDFPDR43uGytO7LPEfFLOkLPJIGWUe_5ps">
+<!--
+<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/search?key=AIzaSyDFPDR43uGytO7LPEfFLOkLPJIGWUe_5ps&zoom=16&q=bar&center=-34.5770135,-58.4367275" allowfullscreen></iframe>
+
+<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=van%20koning%2C%20ca%C3%B1itas&key=AIzaSyDFPDR43uGytO7LPEfFLOkLPJIGWUe_5ps" allowfullscreen></iframe>
+-->
                 </div>
                 <div class="card-content">
                     <span class="card-title activator grey-text text-darken-4">{{ $bar->address }}</span>
